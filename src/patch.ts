9ec3fa07b4d3be56331d1b4344054565796316dd
Sheng-Han (Aysh) Su <13860646+arthow4n@users.noreply.github.com>
2024-03-05 07:39:22 +0100

    fix(patch): extend original `server.moduleGraph` (#5)
    Co-authored-by: Hiroki Osame <hiroki.osame@gmail.com>

--- src/patch.ts
+++ src/patch.ts
@@ -112,20 +112,17 @@ const supportCssModulesHMR = (
 		throw new TypeError('vite:css-analysis plugin transform is not a function');
 	}
 
-	const tag = '?SPOOF?inline';
+	const tag = '?vite-css-modules?inline';
 	viteCssAnalysisPlugin.configureServer = function (server) {
-		const interceptedServer = {
-			moduleGraph: {
-				getModuleById: (id: string) => {
-					const tagIndex = id.indexOf(tag);
-					if (tagIndex !== -1) {
-						id = id.slice(0, tagIndex) + id.slice(tagIndex + tag.length);
-					}
-					return server.moduleGraph.getModuleById(id);
-				},
-			},
+		const { getModuleById } = server.moduleGraph;
+		server.moduleGraph.getModuleById = function (id: string) {
+			const tagIndex = id.indexOf(tag);
+			if (tagIndex !== -1) {
+				id = id.slice(0, tagIndex) + id.slice(tagIndex + tag.length);
+			}
+			return Reflect.apply(getModuleById, this, [id]);
 		};
-		return Reflect.apply(configureServer, this, [interceptedServer]);
+		return Reflect.apply(configureServer, this, [server]);
 	};
 
 	viteCssAnalysisPlugin.transform = async function (css, id, options) {
