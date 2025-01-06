cea4ba9725f51a7adf3baffb2f53f59052285e65
Bill Collins <bill.collins@hp.com>
2025-01-06 10:34:04 +0000

    Ensure files watched

--- src/patch.ts
+++ src/patch.ts
@@ -1,5 +1,5 @@
 import path from 'path';
-import type { Plugin, ServerHook } from 'vite';
+import { DevEnvironment, type Plugin, type ServerHook } from 'vite';
 import type { SourceMap } from 'rollup';
 import { cssModules, cssModuleRE, type PatchConfig } from './plugin/index.js';
 import type { PluginMeta } from './plugin/types.js';
@@ -46,6 +46,9 @@ const supportNewCssModules = (
 
 	viteCssPostPlugin.transform = async function (jsCode, id, options) {
 		if (cssModuleRE.test(id)) {
+			if (this.environment.name === 'dev') {
+				(this.environment as DevEnvironment).pluginContainer.watcher?.add(path.resolve(id));
+			}
 			const inlined = inlineRE.test(id);
 			const info = this.getModuleInfo(id)!;
 			const pluginMeta = info.meta[pluginInstance.name] as PluginMeta | undefined;
